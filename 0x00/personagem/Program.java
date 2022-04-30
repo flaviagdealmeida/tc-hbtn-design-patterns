9741974e22c70c72496efa9d411425198c4ed731
Flávia Almeida <eufga1@gmail.com>
2022-04-30 18:50:48 -0300

    Update Program.java

--- 0x00/personagem/Program.java
+++ 0x00/personagem/Program.java
@@ -1,5 +1,6 @@
 public class Program {
            public static void main(String[] args) {
+            Locale.setDefault(new Locale("pt", "BR"));          
             PersonagemFactory terraMediaPersonagemFactory = new WesterosPersonagemFactory();
             PersonagemFactory westerosPersonagemFactory = new TerraMediaPersonagemFactory();
 
