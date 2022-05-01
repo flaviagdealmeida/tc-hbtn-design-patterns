d8d15fdded15cd4e9d114cf4340a7c5eb48b4fe6
Flávia Almeida <eufga1@gmail.com>
2022-05-01 19:35:24 -0300

    Add files via upload

---
+++ 0x03/media_player/AudioPlayer.java
@@ -0,0 +1,14 @@
+public class AudioPlayer implements MediaPlayer{
+
+    MediaPlayerAdapter mediaPlayerAdapter;
+
+    @Override
+    public void reproduzir(TipoMedia tipo, String nome) {
+       if(tipo.equals(TipoMedia.MP3)) {
+           System.out.println("Reproduzindo MP3: " + nome);
+       } else {
+           mediaPlayerAdapter = new MediaPlayerAdapter(tipo);
+           mediaPlayerAdapter.reproduzir(tipo, nome);
+       }
+    }
+}
