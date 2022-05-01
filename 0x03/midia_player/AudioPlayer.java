7e0281e9d6947a67187e3ecf4759f81fefd6d433
Flávia Almeida <eufga1@gmail.com>
2022-05-01 19:47:38 -0300

    Update AudioPlayer.java

--- 0x03/midia_player/AudioPlayer.java
+++ 0x03/midia_player/AudioPlayer.java
@@ -5,7 +5,7 @@ public class AudioPlayer implements MediaPlayer{
     @Override
     public void reproduzir(TipoMedia tipo, String nome) {
        if(tipo.equals(TipoMedia.MP3)) {
-           System.out.println("Reproduzindo MP3: " + nome);
+           System.out.print("Reproduzindo MP3: " + nome);
        } else {
            mediaPlayerAdapter = new MediaPlayerAdapter(tipo);
            mediaPlayerAdapter.reproduzir(tipo, nome);
