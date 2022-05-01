7d67231c94e58f84654b992a20e44f125227a8d9
Flávia Almeida <eufga1@gmail.com>
2022-05-01 16:20:04 -0300

    Add files via upload

---
+++ 0x03/naves/NaveEspacialDecorator.java
@@ -0,0 +1,19 @@
+public class NaveEspacialDecorator extends NaveEspacial{
+
+    private NaveEspacial nave;
+
+    public NaveEspacialDecorator(NaveEspacial nave ) {
+        this.nave = nave;
+    }
+
+
+    @Override
+    public int getAtaque() {
+        return nave.getAtaque();
+    }
+
+    @Override
+    public int getSaude() {
+        return nave.getSaude();
+    }
+}
