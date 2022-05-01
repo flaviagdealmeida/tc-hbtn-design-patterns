7ee7d6c8bc71d7007d0f92a6326358b33d85afca
Flávia Almeida <eufga1@gmail.com>
2022-05-01 16:15:52 -0300

    Update NanoBotsDecorator.java

--- 0x03/naves/NanoBotsDecorator.java
+++ 0x03/naves/NanoBotsDecorator.java
@@ -1,4 +1,4 @@
-public class NanoBotsDecorator extends NaveEspecialDecorator{
+public class NanoBotsDecorator extends NaveEspacialDecorator{
 
     public NanoBotsDecorator(NaveEspacial naveEspacial) {
         super(naveEspacial);
